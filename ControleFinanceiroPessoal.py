7f0d2a633cf463f97964238d38338d7312c55695
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-04-16 10:32:34 -0300

    test: add transations in main

--- ControleFinanceiroPessoal.py
+++ ControleFinanceiroPessoal.py
@@ -52,7 +52,29 @@ class Carteira:
 
 
 def main():
-    pass
+    transacoes = [
+        Transacao("Compra de café", -10.50, "Alimentação", "15/10/2023"),
+        Transacao("Salário", 2500.00, "Renda", "01/10/2023"),
+        Transacao("Compra de livro", -50.00, "Educação", "20/10/2023"),
+        Transacao("Aluguel", -1200.00, "Moradia", "05/10/2023"),
+        Transacao("Supermercado", -300.00, "Alimentação", "10/10/2023"),
+        Transacao("Venda de bicicleta", 500.00, "Renda Extra", "12/10/2023"),
+        Transacao("Cinema", -30.00, "Lazer", "18/10/2023"),
+        Transacao("Academia", -100.00, "Saúde", "08/10/2023"),
+        Transacao("Freelance", 800.00, "Renda Extra", "22/10/2023"),
+        Transacao("Doação", -50.00, "Caridade", "25/10/2023")
+    ]
+
+    carteira = Carteira()
+
+    for transacao in transacoes:
+        carteira.adicionar(transacao)
+
+    print("\nTransações registradas:")
+    carteira.exibir_transacoes()
+
+    print("\nResumo geral:")
+    carteira.resumo_geral()
 
 if __name__ == "__main__":
     main()
