ece2c638b312a988e4e8fac383ffa6df3bcafebf
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-04-22 11:48:02 -0300

    feat: create Gerente class

--- sistema.de.funcionarios/funcionarios.py
+++ sistema.de.funcionarios/funcionarios.py
@@ -19,6 +19,16 @@ class Funcionario (ABC):
         if salario > 0.0:
             self.__salario = salario
 
+
 class FuncionarioComum(Funcionario):
     def calcular_bonus(self) -> float:
         return self.get_salario * 0.1
+
+
+class Gerente(Funcionario):
+    def __init__(self, nome: str, salario: float, bonus_adicional: float):
+        super().__init__(nome, salario)
+        self.__bonus_adicional = bonus_adicional
+    
+    def calcular_bonus(self):
+        return self.get_salario * 0.2 + self.__bonus_adicional
