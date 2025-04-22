59e78f181ecc426f5f99010d9899eb1e9a6e83ac
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-04-22 16:05:45 -0300

    feat: add logar_acao decotator

--- sistema.de.funcionarios/funcionarios.py
+++ sistema.de.funcionarios/funcionarios.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
-
+    
 class Funcionario (ABC):
     def __init__(self, nome: str, salario: float):
         self.__nome = nome
         self.__salario = salario
     
     @abstractmethod
-    def calcular_bonus() -> float:
+    def calcular_bonus(self) -> float:
         pass
     
     def get_nome(self) -> str:
@@ -19,10 +19,24 @@ class Funcionario (ABC):
         if salario > 0.0:
             self.__salario = salario
 
+def logar_acao(func):
+    def wrapper(*args, **kwargs):
+        self = args[0]
+        bonus = func(*args, **kwargs)
+        cargo = ""
+        
+        if isinstance(self, FuncionarioComum):
+            cargo = "funcionário"
+        elif isinstance(self, Gerente):
+            cargo = "gerenete"
+                
+        return f"O bonus do {cargo} {self.get_nome()} é de {bonus: .2f}"
+    return wrapper
 
 class FuncionarioComum(Funcionario):
+    @logar_acao
     def calcular_bonus(self) -> float:
-        return self.get_salario * 0.1
+        return self.get_salario() * 0.1
 
 
 class Gerente(Funcionario):
@@ -30,5 +44,6 @@ class Gerente(Funcionario):
         super().__init__(nome, salario)
         self.__bonus_adicional = bonus_adicional
     
+    @logar_acao
     def calcular_bonus(self):
-        return self.get_salario * 0.2 + self.__bonus_adicional
+        return self.get_salario() * 0.2 + self.__bonus_adicional
